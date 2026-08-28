rstudio/rstudio

commit 55dc5bdda52991688d5864b074694c94f947f364

Changed behavior of Arrays.asList().toArray() to match new JDK sematics.
Now Arrays.asList(T[]).toArray() returns Object[] instead of T[] to
match toArray() semantics.

The RPC code that serialized lists produced by Arrays.asList() does
not depend anymore on the result type of toArray().

Change-Id: Ic8b2b0326c3f961df6bd0945c12cd4dca48d46cb

user/src/com/google/gwt/user/server/rpc/core/java/util/Arrays.java
@@ -20,10 +20,8 @@ import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 import com.google.gwt.user.server.rpc.ServerCustomFieldSerializer;
 import com.google.gwt.user.server.rpc.impl.DequeMap;
-import com.google.gwt.user.server.rpc.impl.SerializabilityUtil;
 import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.List;
@@ -44,39 +42,17 @@ public final class Arrays {
       return java.util.Arrays.asList().getClass().getName();
     }
 
-    /*
-     * Note: the reason this implementation differs from that of a standard List
-     * (which serializes a number and then each element) is the requirement that
-     * the underlying array retain its correct type across the wire. This gives
-     * toArray() results the correct type, and can generate internal
-     * ArrayStoreExceptions.
-     * 
-     * The type checking is messy because we need some way of converting the
-     * List<X> or related type that we are expecting into the array type that we
-     * are about to try to read. You can't create objects of class Type
-     * directly, so we need to create a dummy array and then use it's class as a
-     * type.
-     */
     public static List<?> instantiate(ServerSerializationStreamReader streamReader,
         Type[] expectedParameterTypes, DequeMap<TypeVariable<?>, Type> resolvedTypes)
         throws SerializationException {
-      Class<?> componentClass = SerializabilityUtil.getClassFromType(expectedParameterTypes[0],
-          resolvedTypes);
-      if (componentClass == null) {
-        return com.google.gwt.user.client.rpc.core.java.util.Arrays.ArrayList_CustomFieldSerializer
+      return com.google.gwt.user.client.rpc.core.java.util.Arrays.ArrayList_CustomFieldSerializer
             .instantiate(streamReader);
-      }
-      
-      Object expectedArray = Array.newInstance(componentClass, 0);
-      Object[] array = (Object[]) streamReader.readObject(expectedArray.getClass(), resolvedTypes);
-      return java.util.Arrays.asList(array);
     }
 
     @Override
     public void deserializeInstance(SerializationStreamReader streamReader, List instance)
         throws SerializationException {
-      com.google.gwt.user.client.rpc.core.java.util.Arrays.ArrayList_CustomFieldSerializer
-          .deserialize(streamReader, instance);
+      // Handled in instantiateInstance.
     }
 
     @SuppressWarnings("unused")
@@ -114,3 +90,4 @@ public final class Arrays {
     }
   }
 }
+

user/src/com/google/gwt/user/client/rpc/core/java/util/Arrays.java
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.user.client.rpc.core.java.util;
 
-import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.client.rpc.CustomFieldSerializer;
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamReader;
@@ -32,20 +31,12 @@ public final class Arrays {
    * Custom field serializer for {@link java.util.Arrays.ArrayList}.
    */
   @SuppressWarnings("rawtypes")
-  public static final class ArrayList_CustomFieldSerializer extends
-      CustomFieldSerializer<List> {
+  public static final class ArrayList_CustomFieldSerializer extends CustomFieldSerializer<List> {
 
     public static String concreteType() {
       return java.util.Arrays.asList().getClass().getName();
     }
 
-    /*
-     * Note: the reason this implementation differs from that of a standard List
-     * (which serializes a number and then each element) is the requirement that
-     * the underlying array retain its correct type across the wire. This gives
-     * toArray() results the correct type, and can generate internal
-     * ArrayStoreExceptions.
-     */
     @SuppressWarnings("unused")
     public static void deserialize(SerializationStreamReader streamReader,
         List<?> instance) throws SerializationException {
@@ -54,21 +45,21 @@ public final class Arrays {
 
     public static List<?> instantiate(SerializationStreamReader streamReader)
         throws SerializationException {
-      Object[] array = (Object[]) streamReader.readObject();
+      int size = streamReader.readInt();
+      Object[] array = new Object[size];
+      for (int i = 0; i < size; ++i) {
+        array[i] = streamReader.readObject();
+      }
       return java.util.Arrays.asList(array);
     }
 
     public static void serialize(SerializationStreamWriter streamWriter,
         List<?> instance) throws SerializationException {
-      Object[] array;
-      if (GWT.isScript()) {
-        // Violator pattern.
-        array = ArraysViolator.getArray0(instance);
-      } else {
-        // Clone the underlying array.
-        array = instance.toArray();
+      int size = instance.size();
+      streamWriter.writeInt(size);
+      for (Object obj : instance) {
+        streamWriter.writeObject(obj);
       }
-      streamWriter.writeObject(array);
     }
 
     @Override

user/super/com/google/gwt/emul/java/util/Arrays.java
@@ -73,12 +73,9 @@ public class Arrays {
       return array.length;
     }
 
-    /*
-     * Semantics are to return an array of identical type.
-     */
     @Override
     public Object[] toArray() {
-      return ArrayHelper.clone(array, 0, array.length);
+      return toArray(new Object[array.length]);
     }
 
     /*

zbynek/gwt

commit add00b44f9920bbfcf2e5765d1916db533a29411

Restore Arrays.ArrayList server-side type check (#10401)
Fixes bugs introduced by 55dc5bdda52991688d5864b074694c94f947f364,
making the check ineffective and making the tests fail.

Also fixed TreeMap test data, and ensure the tests are all run by their
suite.

Fixes #10302

user/src/com/google/gwt/user/server/rpc/core/java/util/Arrays.java
@@ -45,8 +45,13 @@ public final class Arrays {
     public static List<?> instantiate(ServerSerializationStreamReader streamReader,
         Type[] expectedParameterTypes, DequeMap<TypeVariable<?>, Type> resolvedTypes)
         throws SerializationException {
-      return com.google.gwt.user.client.rpc.core.java.util.Arrays.ArrayList_CustomFieldSerializer
-            .instantiate(streamReader);
+      int size = streamReader.readInt();
+      Object[] array = new Object[size];
+      for (int i = 0; i < size; ++i) {
+        Object obj = streamReader.readObject(expectedParameterTypes[0], resolvedTypes);
+        array[i] = obj;
+      }
+      return java.util.Arrays.asList(array);
     }
 
     @Override

user/test/com/google/gwt/user/RpcJreSuite.java
@@ -29,6 +29,7 @@ import com.google.gwt.user.server.rpc.RPCRequestTest;
 import com.google.gwt.user.server.rpc.RPCServletUtilsTest;
 import com.google.gwt.user.server.rpc.RPCTest;
 import com.google.gwt.user.server.rpc.RPCTypeCheckArraysTest;
+import com.google.gwt.user.server.rpc.RPCTypeCheckCollectionsTest;
 import com.google.gwt.user.server.rpc.RPCTypeCheckTest;
 import com.google.gwt.user.server.rpc.RemoteServiceServletTest;
 import com.google.gwt.user.server.rpc.SerializationPolicyLoaderTest;
@@ -69,6 +70,7 @@ public class RpcJreSuite {
     suite.addTestSuite(RPCServletUtilsTest.class);
     suite.addTestSuite(RPCRequestTest.class);
     suite.addTestSuite(RPCTypeCheckArraysTest.class);
+    suite.addTestSuite(RPCTypeCheckCollectionsTest.class);
     suite.addTestSuite(Base64Test.class);
     suite.addTestSuite(UtilTest.class);
     suite.addTestSuite(AbstractXsrfProtectedServiceServletTest.class);

user/test/com/google/gwt/user/server/rpc/RPCTypeCheckCollectionsTest.java
@@ -27,6 +27,7 @@ import junit.framework.TestCase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
@@ -206,7 +207,9 @@ public class RPCTypeCheckCollectionsTest extends TestCase {
       linkedList.add(12345);
       linkedList.add(67890);
       strFactory.write(linkedList);
-      
+      strFactory.write(linkedList);
+      strFactory.write(linkedList);
+
       return strFactory.toString();
       
     } catch (Exception e) {
@@ -851,7 +854,7 @@ public class RPCTypeCheckCollectionsTest extends TestCase {
       linkedHashMap.put("bar", new Integer(12));
       strFactory.write(linkedHashMap);
 
-      TreeMap<HashSet, Integer> treeMap = new TreeMap<HashSet, Integer>();
+      TreeMap<HashSet, Integer> treeMap = new TreeMap<HashSet, Integer>(new IdentitySorter());
       treeMap.put(RPCTypeCheckFactory.generateTestHashSet(), 12345);
       strFactory.write(treeMap);
 
@@ -896,6 +899,12 @@ public class RPCTypeCheckCollectionsTest extends TestCase {
       return null;
     }
   }
+  public static class IdentitySorter implements Comparator<Object>, IsSerializable {
+    @Override
+    public int compare(Object o1, Object o2) {
+      return System.identityHashCode(o1) - System.identityHashCode(o2);
+    }
+  }
 
   @SuppressWarnings("unchecked")
   private static String generateTreeSetHashSetSpoofingSetInteger() {
@@ -908,7 +917,7 @@ public class RPCTypeCheckCollectionsTest extends TestCase {
       hashSet.add(67890);
       strFactory.write(hashSet);
 
-      ReverseSorter sorter = new ReverseSorter();
+      IdentitySorter sorter = new IdentitySorter();
       TreeSet treeSet = new TreeSet(sorter);
       treeSet.add(RPCTypeCheckFactory.generateTestHashSet());
       strFactory.write(treeSet);
@@ -1034,7 +1043,7 @@ public class RPCTypeCheckCollectionsTest extends TestCase {
 
   /**
    * This checks that List correctly handles correct types, and reports when it
-   * gets and incorrectly parameterized generic type.
+   * gets an incorrectly parameterized generic type.
    */
   public void testListSpoofingListRaw() {
     try {

user/test/com/google/gwt/user/server/rpc/RPCTypeCheckFactory.java
@@ -494,10 +494,6 @@ public class RPCTypeCheckFactory {
   }
 
   private void writeListBody(List<?> list) throws SerializationException {
-    if (list.getClass().toString().matches(".*Arrays\\$ArrayList.*")) {
-      write(list.toArray());
-      return;
-    }
     if (!list.getClass().toString().matches(".*Collections\\$SingletonList.*")) {
       bodyString += Integer.toString(list.size()) + RPC_SEPARATOR_CHAR;
     }